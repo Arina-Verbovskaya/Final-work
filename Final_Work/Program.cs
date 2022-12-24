6ef81dd05e4871679b06cc053843d5e5749c6f39
Arina <arina.verbovskaya@mail.ru>
2022-12-24 08:43:15 +0300

    solution added file final work

--- Final_Work/Program.cs
+++ Final_Work/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿char[] variants = { ' ', ',', '.' };
+Console.WriteLine("Введите текст");
+string? text = Console.ReadLine();
+#nullable disable
+string[] array = text.Split(variants);
+Console.WriteLine();
+Console.WriteLine("Ваш текст");
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine();
+Console.WriteLine("Ваши слова c количеством символов меньше или равно 3");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.Write("[" + string.Join(", ", array[i]) + "]");
+    }
+}
+
